d9b5db9bfcdd18b386638852cf1c668788ddafab
Muhammad Shahzaib Balouch <mshahzaibbalouch80@gmail.com>
2023-10-07 14:15:46 +0500

    completed successfully

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Define your routes if needed
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
